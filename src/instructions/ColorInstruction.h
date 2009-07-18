fb2c47e6532a0c00b695ecaf4b05bdf851f73f6f
jonbroFERrealz <jonbro5556@jonathan-brodskys-macbook-pro-44.local>
2009-07-18 11:29:39 -0400

    started putting together a readme. not much yet.
    :"D

---
+++ src/instructions/ColorInstruction.h
@@ -0,0 +1,24 @@
+//
+//  ColorInstruction.h
+//  logo_fighter
+//
+//  Created by jonbroFERrealz on 7/18/09.
+//  Copyright 2009 Heavy Ephemera Industries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "turtle.h"
+#import "Vector2d.h"
+#import "ofMain.h"
+#import "GLPickerView.h"
+#import "GLValuePickerView.h"
+#import "BaseInstruction.h"
+#import "connectionNode.h"
+
+@class BaseInstruction;
+
+@interface ColorInstruction : BaseInstruction {
+	
+}
+
+@end
---
+++ src/instructions/ColorInstruction.mm
@@ -0,0 +1,14 @@
+//
+//  ColorInstruction.mm
+//  logo_fighter
+//
+//  Created by jonbroFERrealz on 7/18/09.
+//  Copyright 2009 Heavy Ephemera Industries. All rights reserved.
+//
+
+#import "ColorInstruction.h"
+
+
+@implementation ColorInstruction
+
+@end
