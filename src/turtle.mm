8ce5ab083416a05b17b5a3573a4c5b5559a4d281
jonbroFERrealz <jonbro5556@jonathan-brodskys-macbook-pro-44.local>
2009-07-21 19:02:31 -0400

    switched the rendering out for a texture map, maybe will give me better graphix?

--- src/turtle.mm
+++ src/turtle.mm
@@ -14,6 +14,7 @@
 #import "ofxMSAShape3D.h"
 
 ofxMSAShape3D *turtleShape;
+ofImage bob;
 
 @implementation Turtle
 
@@ -27,7 +28,7 @@ ofxMSAShape3D *turtleShape;
 
 	pos3 = new Vector3f(0.0, 0.0, 0.0);
 	dir3 = new Vector3f(0.0, 1.0, 0.0);
-	
+	bob.loadImage("bob.png");
 	pos = [[Vector2d alloc] init];
 	pos.x = 160.0;
 	pos.y = 240.0;
@@ -54,14 +55,43 @@ ofxMSAShape3D *turtleShape;
 {
 	//add the left vertex
 	turtleShape->setColor(currentColor.red/255.0, currentColor.green/255.0, currentColor.blue/255.0);
-	turtleShape->addVertex(pos3->x, pos3->y);
+    Vector3f vPoint0(-1.0, -1.0, 0.0f);
+    Vector3f vPoint1( 1.0, -1.0, 0.0f);
+    Vector3f vPoint2( 1.0,  1.0, 0.0f);
+    Vector3f vPoint3(-1.0,  1.0, 0.0f);
+	glPushMatrix();
+	glTranslatef(pos3->x, pos3->y, pos3->z);
+	turtleShape->begin(GL_TRIANGLE_STRIP);
+	bob.getTextureReference().bind();
+	float mat[16];
+	glGetFloatv( GL_MODELVIEW_MATRIX, mat );
+	Vector3f vRight( mat[0], mat[4], mat[8] );
+	Vector3f vUp( mat[1], mat[5], mat[9] );
+	Vector3f vCenter( 0.0f, 0.0f, 0.0f );
+
+	vPoint0 = vCenter + ((-vRight - vUp) * lineWeight);
+	vPoint1 = vCenter + (( vRight - vUp) * lineWeight);
+	vPoint2 = vCenter + (( vRight + vUp) * lineWeight);
+	vPoint3 = vCenter + ((-vRight + vUp) * lineWeight);
+	turtleShape->setTexCoord(0.0, 0.0);
+	turtleShape->addVertex(vPoint0.x,vPoint0.y,vPoint0.z);
+	
+	turtleShape->setTexCoord(1.0f, 0.0f);
+	turtleShape->addVertex(vPoint1.x,vPoint1.y,vPoint1.z);
+	
+	turtleShape->setTexCoord(0.0f, 1.0f);
+	turtleShape->addVertex(vPoint3.x,vPoint3.y,vPoint3.z);
+	
+	turtleShape->setTexCoord(1.0f, 1.0f);
+	turtleShape->addVertex(vPoint2.x,vPoint2.y,vPoint2.z);
+	
+	turtleShape->end();
+	bob.getTextureReference().unbind();
+	glPopMatrix();
 }
 -(void)setLineWidth:(float)_width
 {
 	lineWeight = _width;
-	turtleShape->end();
-	glLineWidth(lineWeight);
-	turtleShape->begin(GL_LINE_STRIP);
 }
 -(void)runFirstInstruction:(BaseInstruction*)_currentInstruction
 {
@@ -94,9 +124,7 @@ ofxMSAShape3D *turtleShape;
 
 	ofSetColor(0x000000);
 	glLineWidth(lineWeight);
-	turtleShape->begin(GL_LINE_STRIP);
 	[self runInstruction:_currentInstruction];
-	turtleShape->end();
 	// save to the photo library
 //	
 	ofFill();
